0ccc5c8f29ca81402176d20eaafdb926ee6e371d
Matt Mueller <muellermr@gmail.com>
2011-05-16 06:53:06 -0500

    Adding galleries endpoint and updating gallery method based on new naming structure

--- lib/montabe/galleries.rb
+++ lib/montabe/galleries.rb
@@ -1,13 +1,20 @@
 module Montabe
   module Galleries
   
+    # Retrieve a list of all galleries
+
+    def galleries
+      response = connection.get("galleries")
+      return_error_or_body(response, response.body.galleries)
+    end
+
     # Retrieve a gallery
     #
     # param [String] gallery_name The name of the gallery
 
     def gallery(gallery_name)
       response = connection.get("galleries/#{gallery_name}")
-      return_error_or_body(response, response.body.galleries)
+      return_error_or_body(response, response.body.gallery)
     end
   end
 end
